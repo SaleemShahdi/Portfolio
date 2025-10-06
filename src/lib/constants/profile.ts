e59e45f227cec74f2ee071738986402c27eb35f2
SaleemShahdi <shadi826@gmail.com>
2025-10-05 21:36:56 -0400

    Some more changes to About Me

--- src/lib/constants/profile.ts
+++ src/lib/constants/profile.ts
@@ -1,11 +1,11 @@
 export const PROFILE_INFO = {
   role: 'Software Engineer, Data & Analytics',
   logo: 'Shahrez Ahmad',
-  slogan: 'Coding, Creativity, Coffee...',
+  slogan: 'Changing the world, one line of code at a time',
   displayName: "Shahrez Ahmad",
   email: "shahrez.ahmad@rutgers.edu",
   about: `
-I'm a software engineer driven by the fundamental belief that computing is the most powerful tool for transformative change. From medicine to engineering, the most interesting challenges are being solved with code, and my passion lies in being part of that solution. I thrive on deconstructing complex problems and building elegant, effective software to solve them.
+I'm a software engineer driven by the fundamental belief that computing is the most powerful tool for transformative change. Spanning industries from medicine and engineering to global commerce, the most interesting challenges are being solved with code, and my passion lies in being part of that solution. I thrive on deconstructing complex problems and building elegant, effective software to solve them.
 
 Feel free to explore my projects to see my work in action, or check out my professional experience.
 `,
