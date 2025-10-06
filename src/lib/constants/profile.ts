c9a0c2fb1c8fe3b844819e7afcc571544d89e6f3
SaleemShahdi <shadi826@gmail.com>
2025-10-05 21:17:46 -0400

    Revised About statement

--- src/lib/constants/profile.ts
+++ src/lib/constants/profile.ts
@@ -5,7 +5,7 @@ export const PROFILE_INFO = {
   displayName: "Shahrez Ahmad",
   email: "shahrez.ahmad@rutgers.edu",
   about: `
-I'm driven by the fundamental belief that computing is the most powerful tool for transformative change. From medicine to engineering, the most interesting challenges are being solved with code, and my passion lies in being part of that solution. I thrive on deconstructing complex problems and building elegant, effective software to solve them.
+I'm a software engineer driven by the fundamental belief that computing is the most powerful tool for transformative change. From medicine to engineering, the most interesting challenges are being solved with code, and my passion lies in being part of that solution. I thrive on deconstructing complex problems and building elegant, effective software to solve them.
 
 Feel free to explore my projects to see my work in action, or check out my professional experience.
 `,
