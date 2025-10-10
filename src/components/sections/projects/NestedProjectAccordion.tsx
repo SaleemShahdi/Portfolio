dceb890fed8119790b0e6abbee00ef5129effa4e
SaleemShahdi <shadi826@gmail.com>
2025-10-09 21:50:57 -0400

    Helped the AI just a bit more

--- src/components/sections/projects/NestedProjectAccordion.tsx
+++ src/components/sections/projects/NestedProjectAccordion.tsx
@@ -43,7 +43,7 @@ const SubAccordion = ({ title, description, content }) => {
           >
             {/* CORRECTED: Removed all classes to let it inherit styles correctly. */}
             <div
-              className="prose min-w-full pb-4 pl-4"
+              className="prose min-w-full pb-4 pl-4 prose-p:leading-8 [&_ul_li+li]:!pl-0"
               dangerouslySetInnerHTML={{ __html: htmlContent }}
             />
           </motion.div>
