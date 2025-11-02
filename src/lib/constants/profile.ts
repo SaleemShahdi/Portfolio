0cf450d5c43704b432086ad8a4d167bffd146d39
SaleemShahdi <shadi826@gmail.com>
2025-11-02 00:49:58 -0400

    Combined separate paragraphs into one in About Me page

--- src/lib/constants/profile.ts
+++ src/lib/constants/profile.ts
@@ -5,8 +5,6 @@ export const PROFILE_INFO = {
   displayName: "Shahrez Ahmad",
   email: "shahrez.ahmad@rutgers.edu",
   about: `
-I'm a software engineer driven by the fundamental belief that computing is the most powerful tool for transformative change. Spanning industries from medicine and engineering to global commerce, the most interesting challenges are being solved with code, and my passion lies in being part of that solution. I thrive on deconstructing complex problems and building elegant, effective software to solve them.
-
-Feel free to explore my projects to see my work in action, or check out my professional experience.
+I'm a software engineer driven by the fundamental belief that computing is the most powerful tool for transformative change. Spanning industries from medicine and engineering to global commerce, the most interesting challenges are being solved with code, and my passion lies in being part of that solution. I thrive on deconstructing complex problems and building elegant, effective software to solve them. Feel free to explore my projects to see my work in action, or check out my professional experience.
 `,
 } as const;
