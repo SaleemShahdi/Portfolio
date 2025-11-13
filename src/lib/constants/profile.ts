f968e21953d023c19eefe1e8a387e2c884e340d7
SaleemShahdi <shadi826@gmail.com>
2025-11-12 23:25:25 -0500

    Referenced nav bar in About Me paragraph

--- src/lib/constants/profile.ts
+++ src/lib/constants/profile.ts
@@ -6,6 +6,6 @@ export const PROFILE_INFO = {
   email: "shahrez.ahmad@rutgers.edu",
   about: `
 I'm a software engineer driven by the fundamental belief that computing is the most powerful tool for transformative change. Spanning industries from medicine and engineering to global commerce, the most interesting challenges are being solved with code, and my passion lies in being part of that solution. I thrive on deconstructing complex problems and building elegant, effective software to solve them.  
-Feel free to explore my projects to see my work in action, or check out my professional experience.
+Use the menu at the top to check out my projects to see my work in action, or to explore my professional experience.
 `,
 } as const;
